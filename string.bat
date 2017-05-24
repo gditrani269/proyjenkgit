93e2b590746374262dbdf255ce387dc6e7cef78f
gditrani269 <gditrani269@gmail.com>
2017-05-24 14:22:49 -0300

    Add files via upload

--- string.bat
+++ string.bat
@@ -1,4 +1,12 @@
+REM los parametros que recibe son tres:
+REM		1 - path completo del repositorio donde se encuentra el entregable 
+REM		2 - lista completa de los archivos que se suben  a dimensions y que conformaran el xml de deploy automatico, deben estar separados por ; y entre comillas.
+REM			por ejemplo: "arch1.arj;arch2.arj;ar_custom.xml"
+REM		3 - Numero de TO
+
 echo off
+Setlocal EnableDelayedExpansion
+
 goto :main
 
 
@@ -50,16 +58,43 @@ ENDLOCAL & SET "inp=%inp%"
 exit /bucle
 ========================================================================
 
-
 :main
-set inp=arsarasa1;arcaca2;artata3
+
+REM Comienzo: crea xml y arma path
+set sOutputFile=XMLImportJar_Homo1.xml
+
+echo ^<import^> >> %sOutputFile%
+echo			^<jar^> >> %sOutputFile%
+
+set fpath=C:\Dimensions\Workspace2\
+set fpath=%fpath:"=%
+set recorte=/
+set recorte=%recorte:"=%
+
+REM set PAR=%CD%
+REM set PAR=C:\Dimensions\Workspace2\ServiciosComunesProductos\AccionesOperatoriaContableSIAC\v1\Entregables
+set PAR=%1
+set PAR=!PAR:%fpath%=%recorte%!
+set PAR=!PAR:\=/!
+
+REM FIN: crea xml y arma path
+
+
+REM set inp=arsarasa1;arcaca2;artata3
+set inp=%2
+REM extrae el primer y ultimo caracter de los parametros ingresados, los cuale sse suponene que son comillas
+set inp=%inp:~1,-1%
+echo inp
+echo %inp%
 
 :repetir
 for /f "tokens=1 delims=; " %%a in ("%inp%") do set out=%%a
 
 echo out
 echo %out%
-@ECHO OFF
+REM escribe en el xml de deploy automatico
+echo				^<path^>D:/Serena/SDA-AGENT/core/var/work/CBUS_OSB%PAR%/!out!^</path^> >> %sOutputFile%
+
 
 call :param %out%
 
@@ -68,6 +103,14 @@ echo %inp%
 
 rem if not %out% == "%inp%" goto :repetir
 if not "%inp%" == "~1" goto :repetir
+
+REM carga numero d eTO en el XML
+set ito=%3
+echo				^<transferencia^>%ito%^</transferencia^> >> %sOutputFile%
+	 		
+
+echo			^</jar^>  >> %sOutputFile%
+echo ^</import^>  >> %sOutputFile%
 
 rem CALL :LENGHT 123456789
 rem ECHO.%LEN%
