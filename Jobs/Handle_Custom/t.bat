37c543e7841d00c25092a34ea4d9bdb35c6a1b1f
gditrani269 <gditrani269@gmail.com>
2018-05-31 11:54:23 -0300

    Add files via upload

---
+++ Jobs/Handle_Custom/t.bat
@@ -0,0 +1,6 @@
+echo ttt.bbb
+
+
+		"C:\Program Files (x86)\Java\jre1.8.0_171\bin\java" -jar C:\Users\l0646482\n\mi_desa\Eclipse\CustomUpDate\CustomUpDate.jar
+		echo errorlevel %errorlevel%
+		exit /b
